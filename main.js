4b339ce917ae2b093306829fff34cc5f3c15008b
AndyLiang1 <andyliang1000@gmail.com>
2021-05-07 15:23:31 -0700

    Small changes

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const card1= document.querySelector('.card__inner');
-const card2= document.querySelector('.card__inner2');
-const card3= document.querySelector('.card__inner3');
+// const card1= document.querySelector('.card__inner');
+// const card2= document.querySelector('.card__inner2');
+// const card3= document.querySelector('.card__inner3');
 
 
 
 
-card1.addEventListener('click', function(){
-    card1.classList.toggle('is-flipped');
-});
+// card1.addEventListener('click', function(){
+//     card1.classList.toggle('is-flipped');
+// });
 
-card2.addEventListener('click', function(){
-    card2.classList.toggle('is-flipped2');
-});
+// card2.addEventListener('click', function(){
+//     card2.classList.toggle('is-flipped2');
+// });
 
-card3.addEventListener('click', function(){
-    card3.classList.toggle('is-flipped3');
-});
+// card3.addEventListener('click', function(){
+//     card3.classList.toggle('is-flipped3');
+// });
 
 
 
