6731c7c5368082cd3b75b61a6ca2a0315884b29a
AndyLiang1 <andyliang1000@gmail.com>
2021-05-08 11:15:29 -0700

    Trying to implement the pop up

--- timer.js
+++ timer.js
@@ -7,6 +7,19 @@ function display_timer(option){
     else if(option.id === "option-2-id"){
         // we know its 50 
     } else {
-        //document.getElementById("popOutBox").timerStyle.display = "block";
+        document.getElementById('pop-out-box').timerStyle.display = "block";
     }
+}
+
+const option3= document.querySelector('option-3');
+option3.addEventListener('click', function(){
+    option3.classList.toggle('boop');
+});
+
+function open_custom_timer(){
+    document.getElementById('pop-out-container').timerStyle.display = "flex";
+}
+
+function close_custom_timer(){
+    document.getElementById('pop-out-container').timerStyle.display = "none";
 }
