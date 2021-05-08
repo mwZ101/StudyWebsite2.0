ab486103e31e798dadf233e0d0bc6eec432647be
AndyLiang1 <andyliang1000@gmail.com>
2021-05-08 16:05:19 -0700

    Got the display status to work. Working on hiding the display timer page when needed.

--- timer.js
+++ timer.js
@@ -1,44 +1,95 @@
 let isPaused = false;
 let isReset = false;
+let isOnStudy = true;
+let isDone = false;
 
 function display_timer(option, studyDur, breakDur){
+    
     let timer = document.getElementById('timer-id');
-    let startingMinutes;
+    let studyingMinutes;
+    let breakMinutes;
     if(option.id === "option-1-id"){
         timer.textContent= "45:00";
-        startingMinutes = 45;
+        studyingMinutes = 45;
+        breakMinutes = 15;
     }
     else if(option.id === "option-2-id"){
         timer.textContent= "50:00";
-        startingMinutes = 50;
+        studyingMinutes = 50;
+        breakMinutes = 10;
     } else {
         timer.textContent= studyDur + ":" + "00";
-        startingMinutes = studyDur;
+        studyingMinutes = studyDur;
+        breakMinutes = breakDur;
+
     }
+    let breakTimeInSeconds= breakMinutes * 60;
+    let studyTimeInSeconds = studyingMinutes * 60;
+ 
+    setInterval(updateCountDown, 1000);
 
-    let time = startingMinutes * 60;
-    setInterval(updateCountdown, 1000);
-
-    function updateCountdown(){
-        if(isReset){
-            time = startingMinutes * 60;
+    function updateCountDown(){
+        if(isDone){
+            document.getElementById('display-study-or-break-status-id').textContent = "You're done!";
+            return;
         }
-        if(!isPaused){
-            console.log("We are not paused");
-            time--;
+        if(isOnStudy){
+            if(isReset){
+                studyTimeInSeconds = studyingMinutes * 60;
+            }
+            if(!isPaused){
+                console.log("We are not paused");
+                studyTimeInSeconds--;
+                if(studyTimeInSeconds === -1){
+                    isOnStudy = false;
+                }
+                
+            }     
+            updateStudyCountDown(studyTimeInSeconds);
+               
+        } else {
+            document.getElementById('display-study-or-break-status-id').textContent = "Break Time Remaining";
+            if(isReset){
+                breakTimeInSeconds = breakMinutes * 60;
+            }
+            if(!isPaused){
+                console.log("We are not paused");
+                breakTimeInSeconds--;
+                if(breakTimeInSeconds === -1){
+                    isDone = true;
+                    return;
+                }
+                
+            }     
+            updateBreakCountDown(breakTimeInSeconds);
         }
-        console.log('isPaused is: ' + isPaused);
-        console.log('isReset is: ' + isReset);
-
-        let minutesRemaining = Math.floor(time/60);
-        let seconds = time % 60;
-
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        timer.textContent = minutesRemaining+":"+seconds;
-        console.log(minutesRemaining+":"+seconds);
     }
 }
 
+function updateStudyCountDown(studyTimeInMinutes){
+   
+    console.log('isPaused is: ' + isPaused);
+    console.log('isReset is: ' + isReset);
+
+    let minutesRemaining = Math.floor(studyTimeInMinutes/60);
+    let seconds = studyTimeInMinutes % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('timer-id').textContent = minutesRemaining+":"+seconds;
+    console.log(minutesRemaining+":"+seconds);
+}
+
+function updateBreakCountDown(breakTimeInMinutes){
+    console.log('isPaused is: ' + isPaused);
+    console.log('isReset is: ' + isReset);
+
+    let minutesRemaining = Math.floor(breakTimeInMinutes/60);
+    let seconds = breakTimeInMinutes % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('timer-id').textContent = minutesRemaining+":"+seconds;
+    console.log(minutesRemaining+":"+seconds);
+}
 function pauseOrContinue(){ 
     if(isPaused === true){
         isPaused = false;
@@ -52,13 +103,21 @@ function pauseOrContinue(){
 }
 
 function reset(){
-    if(isReset === true || isPaused === true){
+    if(isReset === true ){
         return;
     } 
+    if(isReset === false && isPaused === true){
+        isPaused = false;
+    }
     isReset = true;
     pauseOrContinue();
 }
 
+
+
+function back(){
+    document.getElementById('display-timer-id').style.display = "none";
+}
 function open_custom_timer() {
     document.getElementById("pop-out").style.display = "block";
 }
