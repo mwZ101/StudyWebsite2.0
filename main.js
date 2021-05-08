6f4e9573ba67e6121b60c45a952243b9a9d8d6e8
AndyLiang1 <andyliang1000@gmail.com>
2021-05-07 17:23:37 -0700

    Working on generate motiv quote button

--- main.js
+++ main.js
@@ -16,9 +16,72 @@
 // card3.addEventListener('click', function(){
 //     card3.classList.toggle('is-flipped3');
 // });
+"
 
 
 
+function generateQuote(){
+    let quote_array = [
+        "Procrastination is the art of keeping up with yesterday. – Don Marquis",
+        "Procrastination makes easy things hard, hard things harder. – Mason Cooley",
+        "Procrastination is like a credit card: it’s a lot of fun until you get the bill. – Christopher Parker",
+        "Procrastination is opportunity’s assassin. – Victor Kiam",
+        "My biggest regret could be summed up in one word, and that’s procrastination. – Ron Cooper",
+        "Procrastination is the thief of time. – Edward Young",
+        "The secret to getting ahead is getting started.",
+        "The expert in everything was once a beginner.",
+        "Ninety-nine percent of the failures come from people who have the habit of making excuses. – George Washington Carver",
+        "The real man is one who always finds excuses for others, but never excuses himself. – Henry Ward Beecher",
+        "The day you take complete responsibility for yourself, the day you stop making any excuses, that’s the day you start to the top. – O.J. Simpson",
+        "It’s not the time to look for excuses. – Rafael Nadal",
+        "If it’s important to you, you’ll find a way. If it’s not, you’ll find excuses.",
+        "Don’t say you don’t have enough time. You have exactly the same number of hours per day that were given to Helen Keller, Pasteur, Michelangelo, Mother Teresea, Leonardo da Vinci, Thomas Jefferson, and Albert Einstein. – H. Jackson Brown Jr.",
+        "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do. – Pele",
+        "However difficult life may seem, there is always something you can do and succeed at. – Stephen Hawking",
+        "Start where you are. Use what you have. Do what you can. – Arthur Ashe",
+        "There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes. – John Maxwell",
+        "Your positive action combined with positive thinking results in success. – Shiv Khera",
+        "Good things come to people who wait, but better things come to those who go out and get them.",
+        "I find that the harder I work, the more luck I seem to have. – Thomas Jefferson",
+        "Success is the sum of small efforts, repeated day in and day out. – Robert Collier",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Don’t wish it were easier; wish you were better. – Jim Rohn",
+        "The secret of success is to do the common things uncommonly well. – John D. Rockefeller",
+        "Successful and unsuccessful people do not vary greatly in their abilities. They vary in their desires to reach their potential. – John Maxwell",
+        "Perseverance is the hard work you do after you get tired of doing the hard work you already did. – Newt Gingrich",
+        "There is no substitute for hard work. – Thomas Edison",
+        "Self-belief and hard work will always earn you success. – Virat Kohli",
+        "Through hard work, perseverance and a faith in God, you can live your dreams. – Ben Carson",
+        "The only place where success comes before work is in the dictionary. – Vidal Sassoon",
+        "There are no traffic jams on the extra mile. – Zig Ziglar",
+        "There are no shortcuts to any place worth going. – Beverly Sills",
+        "If people only knew how hard I’ve worked to gain my mastery, it wouldn’t seem so wonderful at all. – Michelangelo",
+        "Keep your dreams alive. Understand to achieve anything requires faith and belief in yourself, vision, hard work, determination, and dedication. Remember all things are possible for those who believe. – Gail Devers",
+        "I’ve failed over and over and over again in my life. And that is why I succeed. – Michael Jordan",
+        "If you’re going through hell, keep going. – Winston Churchill",
+        "Failure is the opportunity to begin again more intelligently. – Henry Ford",
+        "You don’t drown by falling in the water; you drown by staying there. – Ed Cole",
+        "I don’t measure a man’s success by how high he climbs, but how high he bounces when he hits the bottom. – George S. Patton",
+        "Patience and perseverance have a magical effect before which difficulties disappear and obstacles vanish. – John Quincy Adams",
+        "By perseverance, the snail reached the ark. – Charles Spurgeon",
+        "Follow your dreams, believe in yourself and don’t give up. – Rachel Corrie",
+        "Don’t let what you cannot do interfere with what you can do. – John Wooden",
+        "It always seems impossible until it’s done.",
+        "Believe you can and you’re halfway there.",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland",
+        "To excel at the highest level – or any level, really – you need to believe in yourself, and hands down, one of the biggest contributors to my self-confidence has been private coaching. – Stephen Curry",
+        "The purpose of human life is to serve and to show compassion and the will to help others. – Albert Schweitzer",
+        "Efforts and courage are not enough without purpose and direction. – John F. Kennedy",
+        "Work gives you meaning and purpose and life is empty without it. – Stephen Hawking",
+        "The whole purpose of education is to turn mirrors into windows. – Sydney J. Harris",
+        "The purpose of our lives is to be happy. – Dalai Lama",
+        "When you dance, your purpose is not to get to a certain place on the floor. It’s to enjoy each step along the way. – Wayne Dyer",
+        "I believe that God has put gifts and talents and ability on the inside of every one of us. When you develop that and you believe in yourself and you believe that you’re a person of influence and a person of purpose, I believe you can rise up out of any situation. – Joel Osteen"
+    ];
 
+    let randomNum = Math.floor(Math.random()* 55);
+    
 
-
+    
+ 
+}
