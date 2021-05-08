24d18e5ea57a4fe00444fd56086725b1aa329034
AndyLiang1 <andyliang1000@gmail.com>
2021-05-08 14:31:00 -0700

    Timer is capable of decrementing

--- timer.js
+++ timer.js
@@ -1,15 +1,51 @@
-
+let isPaused = false;
+let isReset = false;
 
 function display_timer(option, studyDur, breakDur){
-    
+    let timer = document.getElementById('timer-id');
+    let startingMinutes;
     if(option.id === "option-1-id"){
-        // display 45
+        timer.textContent= "45:00";
+        startingMinutes = 45;
     }
     else if(option.id === "option-2-id"){
-        // we know its 50 
+        timer.textContent= "50:00";
+        startingMinutes = 50;
     } else {
-        //custom
+        timer.textContent= studyDur + ":" + breakDur;
+        startingMinutes = studyDur;
     }
+
+    let time = startingMinutes * 60;
+    setInterval(updateCountdown, 1000);
+
+    function updateCountdown(){
+        if(isReset){
+            document.getElementById('button-pause-id').textContent = "Continue";
+        } 
+        
+        if(!isPaused){
+            let minutesRemaining = Math.floor(time/60);
+            let seconds = time % 60;
+
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+            timer.textContent = minutesRemaining+":"+seconds;
+            console.log(minutesRemaining+":"+seconds);
+            time--;
+        }
+    }
+}
+
+function pause(){ 
+    if(document.getElementById('button-pause-id').textContent === "Continue"){
+        document.getElementById('button-pause-id').textContent = "Pause";
+    }
+    isPaused = isPaused ? false : true;
+}
+
+function reset(){
+    isReset= isReset ? false : true;
+    isPaused = isPaused ? false : true;
 }
 
 function open_custom_timer() {
@@ -35,7 +71,7 @@ function save_input() {
     studyID.style.fontSize = "1.8vw";
     breakID.style.fontSize = "1.8vw";
     
-    option3ID.setAttribute("onclick", "displayTimer(this, " + studyMin + ", " + breakMin + ")");
+    option3ID.setAttribute("onclick", "display_timer(this, " + studyMin + ", " + breakMin + ")");
 
     document.getElementById("pop-out").style.display = "none";
 }
