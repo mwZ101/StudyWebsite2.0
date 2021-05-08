2c312ae2fd19c5cebd519c7e4fd40dc8e142b0a4
AndyLiang1 <andyliang1000@gmail.com>
2021-05-07 19:58:11 -0700

    Began work on timer

---
+++ timer.js
@@ -0,0 +1,12 @@
+
+
+function display_timer(option, study_dur, break_dur){
+    if(option.id === "option-1-id"){
+        // display 45
+    }
+    else if(option.id === "option-2-id"){
+        // we know its 50 
+    } else {
+
+    }
+}
